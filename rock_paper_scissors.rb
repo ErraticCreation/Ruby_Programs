a579d3338e405fc8db895ec8441ad8d08c1ddbab
Tarah Nixon <erraticcreation@gmail.com>
2016-02-10 23:06:05 -0500

    Reworked the rock paper scissors code to utilize a mathmatical solution and slim it all down

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,52 +1,45 @@
 def pc_player
   rock_paper_scissors_pc = rand(1..3)
-  if rock_paper_scissors_pc == 1
-    return 'rock'
-  elsif rock_paper_scissors_pc == 2
-    return 'paper'
-  elsif rock_paper_scissors_pc == 3
-    return 'scissors'
+  if (rock_paper_scissors_pc == 1)
+    return [-1, "rock"]
+  elsif (rock_paper_scissors_pc == 2)
+    return [-2, "paper"]
+  elsif (rock_paper_scissors_pc == 3)
+    return [-3, "scissors"]
   end
 end
+
+puts pc_player
 
 def rock_paper_scissors_input
   puts "\n|------Rock Paper Scissors!------|\n\nThe options are:\nRock, Paper, Scissors, or Exit.\nSo, what will it be?"
   rock_paper_scissors = gets.chomp
-  opponent_answer = pc_player()
-  if rock_paper_scissors.downcase == "rock" && opponent_answer == "rock"
-    puts "You: Rock\nOpponent: Rock\n\tIt's a Tie!"
-    play_again()
-  elsif rock_paper_scissors.downcase == "paper" && opponent_answer == "rock"
-    puts "You: Paper\nOpponent: Rock\n\tYou Win!"
-    play_again()
-  elsif rock_paper_scissors.downcase == "scissors" && opponent_answer == "rock"
-    puts "You: Paper\nOpponent: Rock\n\tYou Lost!\n\tBetter Luck Next Time."
-    play_again()
-  elsif rock_paper_scissors.downcase == "rock" && opponent_answer == "paper"
-    puts "You: Rock\nOpponent: Paper\n\tYou Lost!\n\tBetter Luck Next Time."
-    play_again()
-  elsif rock_paper_scissors.downcase == "paper" && opponent_answer == "paper"
-    puts "You: Paper\nOpponent: Paper\n\tIt's a Tie!"
-    play_again()
-  elsif rock_paper_scissors.downcase == "scissors" && opponent_answer == "paper"
-    puts "You: Scissors\nOpponent: Paper\n\tYou Win!"
-    play_again()
-  elsif rock_paper_scissors.downcase == "rock" && opponent_answer == "scissors"
-    puts "You: Rock\nOpponent: Scissors\n\tYou Win!"
-    play_again()
-  elsif rock_paper_scissors.downcase == "paper" && opponent_answer == "scissors"
-    puts "You: Paper\nOpponent: Scissors\n\tYou Lost!\n\tBetter Luck Next Time."
-    play_again()
-  elsif rock_paper_scissors.downcase == "scissors" && opponent_answer == "scissors"
-    puts "You: Scissors\nOpponent: Scissors\n\tIt's a Tie!"
-    play_again()
+  if rock_paper_scissors.downcase == "rock"
+    return [1, "rock"]
+  elsif rock_paper_scissors.downcase == "paper"
+    return [2, "paper"]
+  elsif rock_paper_scissors.downcase == "scissors"
+    return [3, "scissors"]
   elsif rock_paper_scissors.downcase == "exit"
     quit_game()
   else
     puts "Sorry, I didn't understand your anwser."
     sleep(2)
-    rock_paper_scissors_input()
+    play_game()
   end
+end
+
+def play_game
+  pc_answer = pc_player()
+  player_answer = rock_paper_scissors_input()
+  if (pc_answer[0] + player_answer[0] == 0)
+    puts "You: #{player_answer[1]}\nOpponent: #{pc_answer[1]}\n\n\tIt's a Tie!\n"
+  elsif (pc_answer[0] + player_answer[0] == -2) || (pc_answer[0] + player_answer[0] == 1)
+    puts "You: #{player_answer[1]}\nOpponent: #{pc_answer[1]}\n\n\tYou Win!\n"
+  elsif (pc_answer[0] + player_answer[0] == -1) || (pc_answer[0] + player_answer[0] == 2)
+    puts "You: #{player_answer[1]}\nOpponent: #{pc_answer[1]}\n\n\tYou Lost!\n\tBetter Luck Next Time.\n"
+  end
+  play_again()
 end
 
 def play_again
@@ -54,7 +47,7 @@ def play_again
   puts "Would you like to play again?\nThe options are:\n yes or no"
   answer = gets.chomp
   if answer.downcase == "yes"
-    rock_paper_scissors_input()
+    play_game()
   elsif answer.downcase == "no"
     quit_game()
   else
@@ -64,9 +57,9 @@ def play_again
 end
 
 def quit_game
-  puts "|-----Thank you for playing!-----|"
+  puts "\n|-----Thank you for playing!-----|"
   sleep(0.5)
   exit!
 end
 
-rock_paper_scissors_input()
+play_game()
